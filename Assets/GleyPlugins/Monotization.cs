b2a72a63c0b4b4b4736e0a2e478e1e2623df61c0
ALI FARZAN <alifarzantahaa@gmail.com>
2020-10-19 02:26:13 +0500

    Initial commit

---
+++ Assets/GleyPlugins/Monotization.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Monotization : MonoBehaviour {
+
+
+
+
+	
+	public static void LoadAds(){
+
+	}
+
+	public static void ShowAds(){
+
+        Advertisements.Instance.ShowInterstitial();
+    }
+
+	public static void ShowAdsNonFB(){
+
+        Advertisements.Instance.ShowInterstitial();
+    }
+
+	public static void LoadRewardAds(){
+        		
+	}
+
+	public static void ShowRewardAds(){
+
+        Advertisements.Instance.ShowRewardedVideo(CompleteMethod);
+    }
+
+   public static void ShowBannerAd(){
+
+        Advertisements.Instance.ShowBanner(BannerPosition.BOTTOM);
+    }
+
+	public static void HideBannerAd(){
+
+        Advertisements.Instance.HideBanner();
+    }
+
+	public static void BroadcastReward(){
+
+        Advertisements.Instance.ShowRewardedVideo(CompleteMethod);
+    }
+
+    private static void CompleteMethod(bool completed)
+    {
+        if (completed)
+        {
+           
+        }
+        
+    }
+
+
+}
---
+++ Assets/GleyPlugins/contestDialog.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class contestDialog : MonoBehaviour
+{
+    public GameObject panel;
+    public Button okay;
+    public Button cancel;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        okay.onClick.AddListener(O_K);
+        cancel.onClick.AddListener(C_K);
+
+        if (Advertisements.Instance.UserConsentWasSet())
+        {
+            Advertisements.Instance.Initialize();
+        }
+        else {
+            panel.SetActive(true);
+        }
+    }
+
+    private void O_K()
+    {
+        Advertisements.Instance.SetUserConsent(true);
+        Advertisements.Instance.Initialize();
+        panel.SetActive(false);
+    }
+    private void C_K()
+    {
+        Advertisements.Instance.SetUserConsent(false);
+        Advertisements.Instance.Initialize();
+        panel.SetActive(false);
+    }
+}
